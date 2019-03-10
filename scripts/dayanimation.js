ba99c6a09051edc4c65450d900c1d9d742970d29
Hiba <hibaelji@outlook.com>
2019-03-10 19:20:47 +0100

    changed the jquery line to js line (didnt see it)

--- scripts/dayanimation.js
+++ scripts/dayanimation.js
@@ -1,5 +1,6 @@
 // window load event makes sure image is loaded before running animation
-$(window).on("load", function() {
+
+window.onload = function() {
 
 var tl = new TimelineMax({repeat:-1});
 tl.to("#morningclouds, #dayclouds, #eveningclouds, #nightclouds", 45, {backgroundPosition: "-4500px 0px", ease: Linear.easeNone});
@@ -9,4 +10,4 @@ var tl = new TimelineLite();
 tl.to("#bird",15, {left:'100%', ease:Linear.easeNone, repeat: -1});
 //flap the wings of happy the cat
 tl.add(TweenMax.to("#bird", 0.5,{backgroundPosition:"-168.5px 0px", ease:SteppedEase.config(1), repeat:tl.duration()/0.2, repeat: -1}), 0);
-});
+};
